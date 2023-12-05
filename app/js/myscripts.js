cace6be5f7ade36ce40efe40ad1e31a2480b0bde
OlegShyshkovskyi <oleh.shyshkovskyi.pm.2022@lpnu.ua>
2023-12-05 13:29:40 +0200

    lab 4

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -18,14 +18,26 @@ window.onload = () => {
     var ctx = document.getElementById('wavyChart').getContext('2d');
     var data = {
         labels: [25, 50, 75, 100, 125, 150, 175, 200, 225, 250],
-        datasets: [{
-            label: 'Statistics',
-            borderColor: '#4aa59f',
-            backgroundColor: '#86d3ce',
-            data: [2, 3, 17, 12, 55, 15, 28.5, 10, 3, 2],
-            fill: true,
-            lineTension: 0.4,
-        }]
+        datasets: [
+            {
+                label: 'MinStatistics',
+                borderColor: '#3993bb',
+                backgroundColor: '#3993bb',
+                data: [1,1.5,8.5,6,27.5,7.5,14,5,1.5,1],
+                fill: true,
+                lineTension: 0.4,
+                pointRadius: 1
+            },
+            {
+                label: 'MaxStatistics',
+                borderColor: '#86d3ce',
+                backgroundColor: '#86d3ce',
+                data: [2, 3, 17, 12, 55, 15, 28, 10, 3, 2],
+                fill: true,
+                lineTension: 0.4,
+                pointRadius: 1
+            }
+    ]
     };
     var options = {
         responsive: false,
